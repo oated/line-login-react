5ec575e78a4ddeb044c1950d4c0147c7c618c40a
oated <nathapott@gmail.com>
2023-10-26 22:44:15 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useState, useEffect } from 'react'
+import liff from '@line/liff'
+
+function App() {
+
+  const [pictureUrl, setPictureUrl] = useState(viteLogo);
+  const [idToken, setIdToken] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [statusMessage, setStatusMessage] = useState("");
+  const [userId, setUserId] = useState("");
+
+  const logout = () => {
+    liff.logout();
+    window.location.reload();
+  }
+
+  const initLine = () => {
+    liff.init({ liffId: '1655665373-YAopzeO6' }, () => {
+      if (liff.isLoggedIn()) {
+        runApp();
+      } else {
+        liff.login();
+      }
+    }, err => console.error(err));
+  }
+
+  const runApp = () => {
+    const idToken = liff.getIDToken();
+    setIdToken(idToken);
+    liff.getProfile().then(profile => {
+      console.log(profile);
+      setDisplayName(profile.displayName);
+      setPictureUrl(profile.pictureUrl);
+      setStatusMessage(profile.statusMessage);
+      setUserId(profile.userId);
+    }).catch(err => console.error(err));
+  }
+
+  useEffect(() => {
+    initLine();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div style={{ textAlign: "center" }}>
+        <h1>React with LINE Login test bot1</h1>
+        <hr/>
+        <img src={pictureUrl} width="300px" height="300px"/>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>id token: </b> {idToken}</p>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>display name: </b> {displayName}</p>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>status message: </b> {statusMessage}</p>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>user id: </b> {userId}</p>
+
+        <button onClick={() => logout()} style={{ width: "100%", height: 30 }}>Logout</button>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
