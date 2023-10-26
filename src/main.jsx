4c2c42a69a514d9f974e85fe4804f378d8075b05
oated <nathapott@gmail.com>
2023-10-27 01:04:19 +0700

    Router edit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, RouterProvider, Route, Link, createBrowserRouter } from 'react-router-dom'
+import About from './components/About.jsx'
+import Home from './components/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "about",
+    element: <About />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
