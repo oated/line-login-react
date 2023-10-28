6d8749b861bfeeead40484a4008e19c87437e218
oated <nathapott@gmail.com>
2023-10-28 20:57:37 +0700

    fixing

--- src/components/Merchant.jsx
+++ src/components/Merchant.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import viteLogo from '/vite.svg'
 import { useState, useEffect } from 'react'
 import liff from '@line/liff'
+import axios from 'axios'
 
 function Merchant() {
 
@@ -47,49 +48,46 @@ function Merchant() {
     // console.log(detail);
 
     
-    // const axios = require('axios');
-    // let data = JSON.stringify({"market_id": "2", "line_token": "Uc759a2130a40ccadf3ec1798dfab2207"});
-
-    // let config = {
-    // method: 'post',
-    // maxBodyLength: Infinity,
-    // url: 'http://188.166.177.184:3001/bot/add_token_user',
-    // headers: { 'Content-Type': 'application/json', 'Cookie': 'connect.sid=s%3A97EGMK4BdrzqZwYDuOXrKTx-s6WWmklS.FHUkAIB257jH7stTEsn2VcuUMxVsCYmzV%2BdZxSJUHes'},
-    // data : data
-    // };
-
-    // axios.request(config)
-    // .then((response) => {
-    // console.log(JSON.stringify(response.data));
-    // })
-    // .catch((error) => {
-    //     console.log(error);
-    // });
-
-
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ market_id: merchantId,  line_token: userId })
-    };
-    fetch('http://188.166.177.184:3001/bot/add_token_user', requestOptions)
-        .then(async response => {
-            const isJson = response.headers.get('content-type')?.includes('application/json');
-            const data = isJson && await response.json();
-
-            // check for error response
-            if (!response.ok) {
-                // get error message from body or default to response status
-                const error = (data && data.message) || response.status;
-                return Promise.reject(error);
-            }
-
-            this.setState({ market_id: merchantId,  line_token: userId })
+    sendRequestToAPI = () => {
+        const data = JSON.stringify({ market_id: merchantId,  line_token: userId });
+    
+        axios.post('http://188.166.177.184:3001/bot/add_token_user', data, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Cookie': 'connect.sid=s%3A97EGMK4BdrzqZwYDuOXrKTx-s6WWmklS.FHUkAIB257jH7stTEsn2VcuUMxVsCYmzV%2BdZxSJUHes',
+          }
         })
-        .catch(error => {
-            this.setState({ errorMessage: error.toString() });
-            console.error('There was an error!', error);
-        });
+          .then(response => {
+            this.setState({ response: JSON.stringify(response.data) });
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+
+    // const requestOptions = {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({ market_id: merchantId,  line_token: userId })
+    // };
+    // fetch('http://188.166.177.184:3001/bot/add_token_user', requestOptions)
+    //     .then(async response => {
+    //         const isJson = response.headers.get('content-type')?.includes('application/json');
+    //         const data = isJson && await response.json();
+
+    //         // check for error response
+    //         if (!response.ok) {
+    //             // get error message from body or default to response status
+    //             const error = (data && data.message) || response.status;
+    //             return Promise.reject(error);
+    //         }
+
+    //         this.setState({ market_id: merchantId,  line_token: userId })
+    //     })
+    //     .catch(error => {
+    //         this.setState({ errorMessage: error.toString() });
+    //         console.error('There was an error!', error);
+    //     });
   
     useEffect(() => {
       initLine();
@@ -103,7 +101,7 @@ function Merchant() {
         <img src={pictureUrl} width="300px" height="300px" style={{borderRadius: "100rem"}}/>
         <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>display name: </b> {displayName}</p>
         <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>user id: </b> {userId}</p>
-        <button onClick={requestOptions} style={{ width: "100%", height: 30 }}>Queue</button>
+        <button onClick={this.sendRequestToAPI} style={{ width: "100%", height: 30 }}>Queue</button>
         <br />
         <button onClick={() => logout()} style={{ width: "100%", height: 30 }}>Logout</button>
       </div>
