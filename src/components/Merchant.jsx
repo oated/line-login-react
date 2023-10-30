8761953a664b42946bc02f05b5db33824cfaffaa
oated <nathapott@gmail.com>
2023-10-31 02:58:56 +0700

    asd

--- src/components/Merchant.jsx
+++ src/components/Merchant.jsx
@@ -65,9 +65,9 @@ function Merchant() {
             });
     }
 
-    // useEffect(() => {
-    //     initLine();
-    // }, []);
+    useEffect(() => {
+        initLine();
+    }, []);
 
     return (
         <div className="merchant">
