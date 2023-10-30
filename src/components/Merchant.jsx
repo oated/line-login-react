14207b98ef5a4b0e4236c790c635d29df131faec
oated <nathapott@gmail.com>
2023-10-31 02:55:44 +0700

    Fix layout

--- src/components/Merchant.jsx
+++ src/components/Merchant.jsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import liff from '@line/liff';
 import viteLogo from '/vite.svg';
+import profilepic from '/profile-pic.png';
 
 function Merchant() {
     const queryParams = new URLSearchParams(window.location.search);
     const merchantId = queryParams.get("id");
     const merchantName = queryParams.get("name");
     const [idToken, setIdToken] = useState("");
-    const [pictureUrl, setPictureUrl] = useState(viteLogo);
+    const [pictureUrl, setPictureUrl] = useState(profilepic);
     const [displayName, setDisplayName] = useState("");
     const [statusMessage, setStatusMessage] = useState("");
     const [userId, setUserId] = useState("");
@@ -63,19 +64,24 @@ function Merchant() {
             });
     }
 
-    useEffect(() => {
-        initLine();
-    }, []);
+    // useEffect(() => {
+    //     initLine();
+    // }, []);
 
     return (
-        <div className="App">
-            <header className="App-header">
+        <div className="merchant">
+            <header className="header">
+                <div className="nav">
+                    <div className="logo-pro">
+                        <img src="src/assets/TunQ-logo.png" alt="" srcset="" />
+                    </div>
+                    <div className="cus-profile">
+                    <img src={pictureUrl} width="40px" height="40px" style={{ borderRadius: "100%" }} alt="Merchant Image" />
+                    </div>
+                </div>
                 <div style={{ textAlign: "center" }}>
-                    <div><h3>Merchant ID : {merchantId}</h3></div>
-                    <div><h3>Merchant Name : {merchantName}</h3></div>
-                    <img src={pictureUrl} width="300px" height="300px" style={{ borderRadius: "100rem" }} alt="Merchant Image" />
-                    <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>display name: </b> {displayName}</p>
-                    <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>user id: </b> {userId}</p>
+                    <div><h2>{merchantName}</h2></div>
+                    
                     <button onClick={sendRequestToAPI} style={{ width: "100%", height: "auto", backgroundColor: "#000000", color: "#ffffff" }}>Add Queue</button>
                     <br />
                     <button onClick={logout} style={{ width: "100%", height: "auto", backgroundColor: "#000000", color: "#ffffff" }}>Logout</button>
